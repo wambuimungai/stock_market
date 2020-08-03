9319f225307a9f8027d1cd71a7fb34b0d38a55ef
wambuimungai <ijoy.mungai@gmail.com>
2020-08-03 13:05:19 +1000

    restructure the code to allow scraping of multiple companies stock data

--- stock_scraping.py
+++ stock_scraping.py
@@ -1,4 +1,4 @@
-import requests,json, re, csv, os
+import requests,json, re, csv, os, argparse
 from config  import username, password
 from lxml import html
 from bs4 import BeautifulSoup
@@ -11,10 +11,8 @@ login_url = "https://abacus.co.ke/auth/login"
 result = session_request.get(login_url)
 
 tree = html.fromstring(result.text)
-#get the sessions login token
-authenticating_token = list(set(tree.xpath("//input[@name='_token']/@value")))[0]
+authenticating_token = list(set(tree.xpath("//input[@name='_token']/@value")))[0]#get the sessions login token
 #print(authenticating_token)
-
 
 #create payload
 payload = {'username': username,
@@ -29,37 +27,75 @@ result = session_request.post(
 #print(result.ok)
 #print(result.status_code)
 
-#builds url and request JSON data from it
-url = "https://abacus.co.ke/live/company/SCOM/charts"
-result = session_request.get(
-    url,
-    headers = dict(referer=url)
-)
+#get companies to add to url path
+def setup(code_path):
+    with open(code_path) as file:
+        company_codes = [x.rstrip() for x in file]
+
+    return company_codes    
+
+
+# pass the company codes to the url
+def company_url(company_code):
+    #builds url and request JSON data from it
+    url = f"https://abacus.co.ke/live/company/{company_code}/charts"
+ 
+    result = session_request.get(
+        url,
+        headers = dict(referer=url)
+        )
+    return result.content
 
 #extracting JSON data from HTML
-soup = BeautifulSoup(result.content, 'html.parser')
-script = soup.find('script', text=re.compile('Performance = ({.*?});'))
-json_text = re.search(r'Performance = ({.*?});',script.string, flags=re.DOTALL |re.MULTILINE).group(1)
-data = json.loads(json_text)
-#print(data)
-stock_details = data['data']
+def get_json(company_code):
 
-# check if path to file exists
-if not os.path.exists('./output_files'):
-	os.makedirs('./output_files')
+    result = company_url(company_code)
+    if result is not None:
+        soup = BeautifulSoup(result, 'html.parser')
+        script = soup.find('script', text=re.compile('Performance = ({.*?});'))
+        json_text = re.search(r'Performance = ({.*?});',script.string, flags=re.DOTALL |re.MULTILINE).group(1)
+        data = json.loads(json_text)
+        stock_data = data['data']
+        
 
-#write  data to file
-with open('./output_files/SCom_stocks.csv', 'w') as file:
-	# create the csv writer object
-	csvwriter = csv.writer(file)
+    return stock_data
 
-	stock_row = 0
+#Write data to respective company files
+def write_to_file(company_code, stock_data):
+    # check if path to file exists
+    if not os.path.exists(output_dir):
+       os.makedirs(output_dir)
 
-	for stock in stock_details:
-    	if stock_row == 0:
-        	header = stock.keys()
-        	csvwriter.writerow(header)
-        	stock_row += 1
+    #writing data to the output file
+    with open(f'{output_dir}/{company_code}_stocks.csv', 'w') as file:
+        # create the csv writer object
+        csvwriter = csv.writer(file)
 
-    	csvwriter.writerow(stock.values())
+        stock_row = 0
 
+        for stock in stock_data:
+            if stock_row == 0:
+                header = stock.keys()
+        	    csvwriter.writerow(header)
+        	    stock_row += 1
+
+    	    csvwriter.writerow(stock.values())
+
+#fetch the data
+def get_data():
+    for company_code in company_codes:
+        stock_data = get_json(company_code)
+        write_to_file(company_code, stock_data)
+
+#putting it all together
+if __name__== '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--company_code_path', help='Path to the file containing the list of companies to scrape, by default will use companies.txt in the same directory', default='companies.txt')
+    parser.add_argument('--output_dir', help='Path to save the output files in', default='output_files/')
+
+    args = parser.parse_args()
+
+    output_dir = args.output_dir
+    company_codes = setup(args.company_code_path)
+
+    get_data()
